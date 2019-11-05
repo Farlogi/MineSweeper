24b4a22bc870be472eb755517e130140f967f297
Farlogi <Farlogi@users.noreply.github.com>
2019-11-05 18:20:28 +0300

    Make Main menu and Difficult choose

---
+++ src/sample/DifficultMenuController.java
@@ -0,0 +1,29 @@
+package sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class DifficultMenuController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button easyModeButton;
+
+    @FXML
+    private Button mediumModeButton;
+
+    @FXML
+    private Button hardModeButton;
+
+    @FXML
+    void initialize() {
+
+    }
+}
---
+++ src/sample/MainMenuController.java
@@ -0,0 +1,43 @@
+package sample;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class MainMenuController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button startGameButton;
+
+    @FXML
+    void initialize() {
+        startGameButton.setOnAction(actionEvent -> {
+            startGameButton.getScene().getWindow().hide();
+            try {
+                Parent root = FXMLLoader.load(getClass().getResource("difficultMenu.fxml"));
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root));
+                stage.getIcons().add(new Image("file:src/sample/resources/img/icon.png"));
+                stage.setTitle("Minesweeper");
+                stage.setResizable(false);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+    }
+}
